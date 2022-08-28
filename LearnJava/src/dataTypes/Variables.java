bc00c679d7fa44218e9cd45cf23331e37a859257
VaideeshwaranS <itisvaidee@hotmail.com>
2022-08-28 13:22:24 +0530

    Commiting the Data Types and Variables

---
+++ LearnJava/src/dataTypes/Variables.java
@@ -0,0 +1,46 @@
+package dataTypes;
+
+public class Variables {
+
+	public static void main(String args[]) {
+		
+		/*
+		String Name = ""; // there is diff in "" and null
+		 
+		
+		String Specialchar = "ABCDE abcde 1234567 \\ \" \n \t \b !@#$%^&*()"; 
+		
+		
+		System.out.println(Specialchar);
+		*/
+	
+		/*
+		int a,b;
+		a = b = 10;
+		a = a + 10 ;
+		
+		System.out.println(a + b);
+		 */
+		/*
+		int a = 10; 
+		double dec = 10.0000000005;
+		String name = "Jency";
+		System.out.println(dec + a + name);
+		
+		String name = "hello sdfkjsndg lkjmsd lkansdlkmas alskmd alaksd lksdmdasl kmms dflkkdslk  okjasklfj s flksjdfsldk j";
+		int a = 1; 
+		char a1 = 'a';
+		System.out.println(name);
+		
+		*/
+		
+		
+		final int  totalSalary = 10000; // constant
+		
+		// Identifier is about naming a variable. It should have a meaningful name to use in later part of program
+		System.out.println(totalSalary);
+		
+		
+	}
+
+}
