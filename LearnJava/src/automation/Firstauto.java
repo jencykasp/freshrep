fc92ef9b1be8a195ed3773b7dd6c8d86a950f393
jencykasp <jency.kasp@gmail.com>
2022-10-03 02:36:39 +0530

    Merge branch 'Jency' of https://github.com/jencykasp/freshrep.git into Jency commit

---
+++ LearnJava/src/automation/Firstauto.java
@@ -0,0 +1,23 @@
+package automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Firstauto {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		
+System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");	
+WebDriver driver= new ChromeDriver();
+
+driver.get("https://www.saucedemo.com/");
+driver.findElement(By.id("user-name")).sendKeys("standard_user");
+driver.findElement(By.id("password")).sendKeys("secret_sauce");
+driver.findElement(By.name("login-button")).click();
+driver.quit();
+	}
+
+}
