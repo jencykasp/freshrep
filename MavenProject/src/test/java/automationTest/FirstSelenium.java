74e725e8bbe94d7d83866cacb6e4fb30e5e9744f
VaideeshwaranS <itisvaidee@hotmail.com>
2022-10-02 16:50:58 +0530

    FirstProgram and Dependencies added in pom.xml

---
+++ MavenProject/src/test/java/automationTest/FirstSelenium.java
@@ -0,0 +1,28 @@
+package automationTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSelenium {
+
+	public static void main(String[] args) {
+		
+		// I refer Selenium as driver in my programs..
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+		// System.setProperty("webdriver.gecko.driver", "D:\\Drivers\\geckodriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.saucedemo.com/");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		
+		driver.findElement(By.name("login-button")).click();
+		
+		driver.quit();
+	}
+
+}
