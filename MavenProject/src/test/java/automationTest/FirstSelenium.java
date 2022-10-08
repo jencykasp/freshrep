6edf6480ad4cb1b05c838983589e1d1ae151ef7a
VaideeshwaranS <itisvaidee@hotmail.com>
2022-10-08 13:55:43 +0530

    TestNG Added to maven

--- MavenProject/src/test/java/automationTest/FirstSelenium.java
+++ MavenProject/src/test/java/automationTest/FirstSelenium.java
@@ -3,26 +3,46 @@ package automationTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 
 public class FirstSelenium {
 
-	public static void main(String[] args) {
-		
-		// I refer Selenium as driver in my programs..
-		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
-		// System.setProperty("webdriver.gecko.driver", "D:\\Drivers\\geckodriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.saucedemo.com/");
-		
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		
-		driver.findElement(By.name("login-button")).click();
-		
-		driver.quit();
-	}
-
+@Test
+public void MyFirstTestCase() {
+	System.out.println("First Test");
 }
+
+	@Test
+	public void MySecondTest() {
+		System.out.println("Second Test");
+	}
+}
+
+
+// 100 TC  - 100 main programs
+//Test Framework - To Organize the tests in automation and to Run Test using TestRunner
+// Also geneerates emailable Test Report 
+// JUnit - stable and very old 
+// TestNg - Next Gen Testing Framework for Java
+
+
+
+/*
+//I refer Selenium as driver in my programs..
+	System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+	// System.setProperty("webdriver.gecko.driver", "D:\\Drivers\\geckodriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+
+	driver.get("https://www.saucedemo.com/");
+
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+	driver.findElement(By.name("login-button")).click();
+
+	driver.quit();
+
+*/
