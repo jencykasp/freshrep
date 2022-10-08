c74c3f532658adddcfda814d1f17844e01c25355
Vaideeshwaran S <itisvaidee@hotmail.com>
2022-10-08 11:45:38 +0530

    Update Inheritfixerror.java

--- LearnJava/src/sample/Inheritfixerror.java
+++ LearnJava/src/sample/Inheritfixerror.java
@@ -4,9 +4,9 @@ public class Inheritfixerror {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Y i = new Y(3);
+		Y obj = new Y(3);
 
-		System.out.println(i);
+		System.out.println(obj);
 	}
 
 }
