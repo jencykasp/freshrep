5dbada0c8718d8cea3c69cf04b0ed788b1aa413a
jencykasp <jency.kasp@gmail.com>
2022-10-16 13:35:45 +0530

    COMMIT

--- LearnJava/src/automation/Firstauto.java
+++ LearnJava/src/automation/Firstauto.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Firstauto {
 
 	public static void main(String[] args) {
-// TODO Auto-generated method stub
-		
+// TODO Auto-generated method stub	
 System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");	
 WebDriver driver= new ChromeDriver();
 
--- Maven/src/test/java/automationtesting/Firstauto.java
+++ Maven/src/test/java/automationtesting/Firstauto.java
@@ -3,13 +3,34 @@ package automationtesting;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
 public class Firstauto {
 
-	public static void main(String[] args) {
-// TODO Auto-generated method stub
-		
+@Test	
+public void myfirsttestcase()
+{
+	System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");	
+	WebDriver driver= new ChromeDriver();
+
+	driver.get("https://www.saucedemo.com/");
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	driver.findElement(By.name("login-button")).click();
+	driver.quit();
+	
+}
+	
+	
+@Test
+public void mysecondtestcase()
+{
+
+	System.out.println("It decor room" );
+}
+}
+/*
 System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");	
 WebDriver driver= new ChromeDriver();
 
@@ -18,6 +39,4 @@ driver.findElement(By.id("user-name")).sendKeys("standard_user");
 driver.findElement(By.id("password")).sendKeys("secret_sauce");
 driver.findElement(By.name("login-button")).click();
 driver.quit();
-	}
-
-}
+*/
