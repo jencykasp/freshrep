d6cf07fb06d8426d18cf58653795d8ba0920ebb2
jencykasp <jency.kasp@gmail.com>
2022-11-05 10:47:55 +0530

    first auto

--- Maven/src/test/java/automationtesting/Firstauto.java
+++ Maven/src/test/java/automationtesting/Firstauto.java
@@ -2,8 +2,13 @@ package automationtesting;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import pageobj.Inventory;
+import pageobj.Login;
+import pageobj.productlink;
 
 
 public class Firstauto {
@@ -15,16 +20,24 @@ public void myfirsttestcase()
 	WebDriver driver= new ChromeDriver();
 
 	driver.get("https://www.saucedemo.com/");
-	driver.findElement(By.id("user-name")).sendKeys("standard_user");
-	driver.findElement(By.id("password")).sendKeys("secret_sauce");
-	driver.findElement(By.name("login-button")).click();
-	driver.quit();
 	
+	Login log= new Login(driver);
+	log.enterusername("standard_user");
+	log.enterpassword("secret_sauce");
+	log.clicklogin();
+
+	Inventory invent= new Inventory(driver);
+	System.out.println(invent.verify());
+	
+	productlink prod= new productlink(driver);
+	//System.out.println(prod.clickproduct());
+prod.clickproduct();
+	driver.quit();
 }
 	
-	
-@Test
-public void mysecondtestcase()
+}	
+//@Test
+/*public void mysecondtestcase()
 {
 
 	System.out.println("It decor room" );
